9b5a6d7cabb124454c32678dbf4eccbb1b9cddbb
XILE-CAI <c358143455@gmail.com>
2022-05-10 16:29:14 +0930

    assignment5

---
+++ lists-cond-assignment-problem/app.js
@@ -0,0 +1,25 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            enteredTask:'',
+            tasks:[],
+            taskListIsVisible: true
+        };
+    },
+    computed: {
+        buttonCaption() {
+          return this.taskListIsVisible ? 'Hide List' : 'Show List';
+        },
+      },
+    methods:{
+        addTask(){
+            this.tasks.push(this.enteredTask);
+        },
+        toggleTaskList(){
+            this.taskListIsVisible = !this.taskListIsVisible;
+        }
+    }
+})
+
+app.mount('#assignment')
+
